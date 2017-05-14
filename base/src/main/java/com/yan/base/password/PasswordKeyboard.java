1371fb927e822c1ab2a22413eff3f089c43628ee
yan493483722 <493483722@qq.com>
2017-05-14 23:03:53 +0800

    set  up keyboard

--- base/src/main/java/com/yan/base/password/PasswordKeyboard.java
+++ base/src/main/java/com/yan/base/password/PasswordKeyboard.java
@@ -8,7 +8,13 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by YanZi on 2017/5/2.
@@ -16,7 +22,7 @@ import android.view.WindowManager;
  * modify:
  * modify date:
  */
-public class PasswordKeyboard extends View {
+public class PasswordKeyboard extends View implements View.OnClickListener{
 
     private static final String clear_all = "清空";
     private static final String delete = "删除";
@@ -32,6 +38,10 @@ public class PasswordKeyboard extends View {
 
     private Paint mLinePaint;
 
+    //List集合存储Key,方便每次输错都能再次随机数字键盘
+    private final List<TextView> keyButtons = new ArrayList<>();
+
+//    private List<String>
 
     //    第一个构造函数：     当不需要使用xml声明或者不需要使用inflate动态加载时候，实现此构造函数即可
     public PasswordKeyboard(Context context) {
@@ -60,16 +70,42 @@ public class PasswordKeyboard extends View {
 
 
     private void init() {
+
+        divLineWidth = PasswordInputView.dip2px(getContext(), 2);
+
         keyWidth = (screenWidth - 2 * divLineWidth) / 3;
         keyHeight = (int) (keyWidth * (1 - 0.618));
 
 
-        divLineWidth = PasswordInputView.dip2px(getContext(), 2);
         if (mLinePaint == null) {
             mLinePaint = new Paint();
             mLinePaint.setColor(Color.parseColor("#cccccc"));
             mLinePaint.setStrokeWidth(divLineWidth);
         }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+//            setBackground(new ColorDrawable(Color.GRAY));
+//        }else{
+//            setBackgroundDrawable(new ColorDrawable(Color.GRAY));
+//        }
+
+//        for (int i = 0; i < 12; i++) {
+//            Button item = new Button(getContext());
+//            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(keyWidth, keyHeight);
+//            item.setLayoutParams(params);
+//            item.setOnClickListener(this);
+//            item.setText(keyList.get(i));
+//            item.setBackgroundDrawable(getResources().getDrawable(R.drawable.key_selector));
+//            //监听"删除"的长按监听事件,完成重复删除操作
+//            if (DEL.equals(keyList.get(i))) {
+//                item.setOnTouchListener(this);
+//            }
+//            item.setTag(keyList.get(i));
+//            addView(item);
+//            keyButtons.add(item);
+//        }
+
+
+
     }
 
 
@@ -79,26 +115,24 @@ public class PasswordKeyboard extends View {
     }
 
 
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        //画横线
-        canvas.drawLine(0,0, getMeasuredWidth(),0, mLinePaint);
-        //画竖线
-        for (int i = 1; i < 4; i++) {
-            if (i != 3) {
-                int index_x = keyWidth * i + divLineWidth * (i - 1);
+        for (int i = 0; i < 4; i++) {
+            //画竖线
+            if (i < 2) {
+                int index_x = keyWidth *(i+1)+ divLineWidth / 2;
                 canvas.drawLine(index_x, 0, index_x, getMeasuredHeight(), mLinePaint);
             }
             //画横线
-            int index_y = (divLineWidth + keyHeight) * i;
+            int index_y = (divLineWidth + keyHeight) * i + divLineWidth / 2;
             canvas.drawLine(0, index_y, getMeasuredWidth(), index_y, mLinePaint);
         }
-
-
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }

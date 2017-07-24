9a6c741d2420535c2b44c9304b0e69f7cdd60eb7
yanzi <493483722@qq.com>
2017-07-24 18:05:04 +0800

    主题初研究

--- app/src/main/java/com/yan/basedemo/aty/bar/NormalStatusBarAty.java
+++ app/src/main/java/com/yan/basedemo/aty/bar/NormalStatusBarAty.java
@@ -37,12 +37,13 @@ public class NormalStatusBarAty extends BaseAty {
                 onBackPressed();
             }
         });
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            //透明状态栏
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            //透明导航栏
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        }
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            //透明状态栏
+//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//            //透明导航栏
+//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+//        }
 
     }
 

96e097371c59830a309497d1b7b6be8ee31ac2c2
yanzi <493483722@qq.com>
2017-07-28 18:01:34 +0800

    沉浸式状态栏初步研究

--- app/src/main/java/com/yan/basedemo/aty/bar/ImgStatusBarAty.java
+++ app/src/main/java/com/yan/basedemo/aty/bar/ImgStatusBarAty.java
@@ -1,7 +1,14 @@
 package com.yan.basedemo.aty.bar;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import com.yan.base.BaseAty;
+import com.yan.base.widget.BaseToolbar;
 import com.yan.basedemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by YanZi on 2017/7/24.
@@ -10,18 +17,40 @@ import com.yan.basedemo.R;
  * modify date:
  */
 public class ImgStatusBarAty extends BaseAty {
+    @BindView(R.id.btb_status_bar_img)
+    BaseToolbar btbStatusBarImg;
+    @BindView(R.id.img_status)
+    ImageView imgStatus;
+
     @Override
     protected void initContentView() {
         setContentView(R.layout.aty_img_status_bar);
+        ButterKnife.bind(this);
     }
 
     @Override
     protected void initView() {
+        setBaseToolbar(btbStatusBarImg, true);
+        btbStatusBarImg.setTitleText("全屏的状态栏");
+        btbStatusBarImg.setRightText("全屏");
 
+        btbStatusBarImg.setBaseToolbarListener(new BaseToolbar.BaseToolbarListener() {
+            @Override
+            public void clickLeft() {
+                onBackPressed();
+            }
+
+            @Override
+            public void clickRight() {
+
+            }
+        });
     }
 
     @Override
     public void initData() {
 
     }
+
+
 }
--- base/src/main/java/com/yan/base/BaseAty.java
+++ base/src/main/java/com/yan/base/BaseAty.java
@@ -8,8 +8,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.view.ViewCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -152,13 +152,7 @@ public abstract class BaseAty extends AppCompatActivity implements PermissionLis
                     break;
                 case BaseToolbar.STATUS_BAR_TYPE_IMG:
                     setTheme(R.style.AppTheme);
-                    View decorView = getWindow().getDecorView();
-                    int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-                    decorView.setSystemUiVisibility(option);
-                    getWindow().setStatusBarColor(Color.TRANSPARENT);
-                    ActionBar actionBar = getSupportActionBar();
-                    actionBar.hide();
+                    translucentStatusBar(false);
 
 //                    toolbar.tb_base_tb.setPopupTheme(R.style.AppThemeFull);
                     break;
@@ -166,22 +160,26 @@ public abstract class BaseAty extends AppCompatActivity implements PermissionLis
 
                     break;
                 case BaseToolbar.STATUS_BAR_TYPE_FULL:
+                    //保持底部
                     setTheme(R.style.AppThemeFull);
                     Window window = getWindow();
-
+                    //去除透明的status bar
                     window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                    //绘制新的status 颜色
                     window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                    //设置新的status bar 颜色
                     window.setStatusBarColor(toolbar.getBackgroundColor());
+                    //设置decorView 的系统状态栏为可见状态
                     window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
-                    toolbar.tb_base_tb.setFitsSystemWindows(true);
+                    //设置 toolbar 自适应
                     ViewGroup mContentView = (ViewGroup) window.findViewById(Window.ID_ANDROID_CONTENT);
                     View mChildView = mContentView.getChildAt(0);
                     if (mChildView != null) {
+                        //设置 设置窗体自适应 自适应
                         ViewCompat.setFitsSystemWindows(mChildView, false);
-                        ViewCompat.requestApplyInsets(mChildView);
+                        //设置requestFitSystemWindows后请求一个新的调度
+                        ViewCompat.requestApplyInsets(mChildView);//是否需要调用？嵌套了?
                     }
-
-
 
                     //取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
                     getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -203,9 +201,68 @@ public abstract class BaseAty extends AppCompatActivity implements PermissionLis
 
                     break;
             }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTheme(R.style.AppTheme);
 
+            switch (toolbar.getBaseToolBarType()) {
+                case BaseToolbar.STATUS_BAR_TYPE_NORMAL:
+
+                    break;
+                case BaseToolbar.STATUS_BAR_TYPE_IMG:
+
+                    break;
+                case BaseToolbar.STATUS_BAR_TYPE_NO:
+
+                    break;
+                case BaseToolbar.STATUS_BAR_TYPE_FULL:
+                    Window window = getWindow();
+                    window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                    ViewGroup mContentView = (ViewGroup) window.findViewById(Window.ID_ANDROID_CONTENT);
+                    View mContentChild = mContentView.getChildAt(0);
+//                    int statusBarHeight = getStatusBarHeight(activity);
+//
+//                    removeFakeStatusBarViewIfExist(activity);
+//                    addFakeStatusBarView(activity, statusColor, statusBarHeight);
+//                    addMarginTopToContentChild(mContentChild, statusBarHeight);
+
+                    if (mContentChild != null) {
+                        ViewCompat.setFitsSystemWindows(mContentChild, false);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    /**
+     * translucentStatusBar(full-screen)
+     * <p>
+     * 1. set Flags to full-screen
+     * 2. set FitsSystemWindows to false
+     *
+     * @param hideStatusBarBackground hide statusBar's shadow
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    void translucentStatusBar(boolean hideStatusBarBackground) {
+        Window window = getWindow();
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        if (hideStatusBarBackground) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        } else {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
         }
 
+        ViewGroup mContentView = (ViewGroup) window.findViewById(Window.ID_ANDROID_CONTENT);
+        View mChildView = mContentView.getChildAt(0);
+        if (mChildView != null) {
+            ViewCompat.setFitsSystemWindows(mChildView, false);
+            ViewCompat.requestApplyInsets(mChildView);
+        }
     }
 
     /**
@@ -243,6 +300,7 @@ public abstract class BaseAty extends AppCompatActivity implements PermissionLis
      * @param permissions
      * @param requestCode
      */
+
     public void requestPermission(String[] permissions, int requestCode) {
         mPermissionManager.requestPermission(permissions, requestCode);
     }

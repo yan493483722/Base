cf5e221b3ebe5504e0ee0cce0af4e1fe0d770b90
yanzi <493483722@qq.com>
2017-08-02 15:10:18 +0800

    状态栏深度定制

--- base/src/main/java/com/yan/base/toolbar/BaseToolbarUtil.java
+++ base/src/main/java/com/yan/base/toolbar/BaseToolbarUtil.java
@@ -47,13 +47,12 @@ public class BaseToolbarUtil {
                 break;
             case BaseToolbar.STATUS_BAR_TYPE_IMG_NORMAL:
                 baseAty.setTheme(R.style.AppTheme);
-                translucentStatusBar(baseAty, false);
-                toolbar.tb_base_tb.setFitsSystemWindows(true);
+                setImageStatusBar(toolbar, baseAty, false);
+
                 break;
             case BaseToolbar.STATUS_BAR_TYPE_IMG_FULL:
                 baseAty.setTheme(R.style.AppTheme);
-                translucentStatusBar(baseAty, true);
-                toolbar.tb_base_tb.setFitsSystemWindows(true);
+                setImageStatusBar(toolbar, baseAty, true);
                 break;
             default:
                 break;
@@ -94,34 +93,20 @@ public class BaseToolbarUtil {
     }
 
 
-    /**
-     * translucentStatusBar(full-screen)
-     * <p>
-     * 1. set Flags to full-screen
-     * 2. set FitsSystemWindows to false
-     *
-     * @param hideStatusBarBackground hide statusBar's shadow
-     */
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    static void translucentStatusBar(Activity aty, boolean hideStatusBarBackground) {
+    static void setImageStatusBar(BaseToolbar toolbar, Activity aty, boolean isNormal) {
         Window window = aty.getWindow();
-
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        if (hideStatusBarBackground) {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        } else {
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
-        }
-
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(isNormal ? Color.TRANSPARENT : aty.getResources().getColor(R.color.transparent_half));
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         ViewGroup mContentView = (ViewGroup) window.findViewById(Window.ID_ANDROID_CONTENT);
         View mChildView = mContentView.getChildAt(0);
         if (mChildView != null) {
             ViewCompat.setFitsSystemWindows(mChildView, false);
             ViewCompat.requestApplyInsets(mChildView);
         }
+        toolbar.tb_base_tb.setFitsSystemWindows(true);
     }
 
     /**

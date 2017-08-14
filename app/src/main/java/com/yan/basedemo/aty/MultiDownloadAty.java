a6f14b98a0837475a174870973f7bf24f771bcca
yanzi <493483722@qq.com>
2017-08-14 18:01:43 +0800

    标题设置

--- app/src/main/java/com/yan/basedemo/aty/MultiDownloadAty.java
+++ app/src/main/java/com/yan/basedemo/aty/MultiDownloadAty.java
@@ -1,11 +1,9 @@
 package com.yan.basedemo.aty;
 
-import android.os.Build;
-import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 
 import com.yan.base.BaseAty;
+import com.yan.base.toolbar.BaseToolbar;
 import com.yan.basedemo.R;
 
 import butterknife.BindView;
@@ -20,10 +18,26 @@ import butterknife.ButterKnife;
 public class MultiDownloadAty extends BaseAty {
 
 
+    @BindView(R.id.btb_multi_download)
+    BaseToolbar btbMultiDownload;
+    @BindView(R.id.rc_multi_download)
+    RecyclerView rcMultiDownload;
+
     @Override
     protected void initContentView() {
         setContentView(R.layout.aty_multi_download);
         ButterKnife.bind(this);
+        btbMultiDownload.setBaseToolbarListener(new BaseToolbar.BaseToolbarListener() {
+            @Override
+            public void clickLeft(int type) {
+                onBackPressed();
+            }
+
+            @Override
+            public void clickRight(int type) {
+
+            }
+        });
     }
 
     @Override
@@ -35,4 +49,5 @@ public class MultiDownloadAty extends BaseAty {
     public void initData() {
 
     }
+
 }

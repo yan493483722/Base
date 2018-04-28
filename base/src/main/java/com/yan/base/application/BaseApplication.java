ad1aacaa85203232925cba318ae472da0d1b4a84
yanzi <493483722@qq.com>
2018-04-28 14:51:18 +0800

    android 3.1兼容

--- base/src/main/java/com/yan/base/application/BaseApplication.java
+++ base/src/main/java/com/yan/base/application/BaseApplication.java
@@ -17,7 +17,7 @@ import com.yan.base.BuildConfig;
  * modify desc:
  */
 
-public abstract class BaseApplication extends Application {
+public  class BaseApplication extends Application {
 
     /**
      * 用来保存当手机屏幕高度
@@ -48,9 +48,9 @@ public abstract class BaseApplication extends Application {
         //TODO　database
     }
 
-    protected abstract void initNetWork();
+    protected  void initNetWork(){};
 
-    protected abstract void initOther();
+    protected  void initOther(){};
 
     private void initLogger() {
         Log.e("BaseApplication", "BaseApplication BuildConfig.DEBUG" + BuildConfig.DEBUG);
